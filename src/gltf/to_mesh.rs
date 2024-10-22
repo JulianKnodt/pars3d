f80a558ee2eb7dfaa4a488957519b78873a16089
julianknodt <julianknodt@gmail.com>
2024-10-21 19:24:58 -0700

    Remove an unused and invalid conversion

--- src/gltf/to_mesh.rs
+++ src/gltf/to_mesh.rs
@@ -3,49 +3,6 @@ use crate::mesh::{Mesh, Node, Scene, Skin};
 
 use crate::{tform_point, FaceKind};
 
-/// Convert a GLTF Scene into a flat mesh.
-/// Will put the mesh into it's default pose.
-impl From<GLTFScene> for Mesh {
-    fn from(gltf_scene: GLTFScene) -> Self {
-        let mut out = Self::default();
-        gltf_scene.traverse(&mut |node, tform| {
-            let Some(mi) = node.mesh else {
-                return;
-            };
-            let mut mesh = gltf_scene.meshes[mi].clone();
-
-            out.face_mesh_idx.extend((0..mesh.f.len()).map(|_| mi));
-            let fs = mesh
-                .f
-                .iter()
-                .map(|&vis| vis.map(|vi| vi + out.v.len()))
-                .map(FaceKind::Tri);
-            out.f.extend(fs);
-            let curr_num_v = out.v.len();
-            out.v
-                .extend(mesh.v.into_iter().map(|v| tform_point(tform, v)));
-            out.n.append(&mut mesh.n);
-            out.uv[0].append(&mut mesh.uvs);
-            if mesh.joint_idxs.is_empty() {
-                assert!(mesh.joint_weights.is_empty());
-                out.joint_idxs
-                    .extend((curr_num_v..out.v.len()).map(|_| [0; 4]));
-                out.joint_weights
-                    .extend((curr_num_v..out.v.len()).map(|_| [0.; 4]));
-            } else {
-                assert!(!mesh.joint_weights.is_empty());
-                out.joint_idxs.append(&mut mesh.joint_idxs);
-                out.joint_weights.append(&mut mesh.joint_weights);
-            }
-        });
-        // flip all UV
-        for uv in &mut out.uv[0] {
-            uv[1] = 1. - uv[1];
-        }
-        out
-    }
-}
-
 impl From<GLTFMesh> for Mesh {
     fn from(gltf_mesh: GLTFMesh) -> Self {
         let GLTFMesh {
@@ -93,6 +50,7 @@ impl From<GLTFScene> for Scene {
         }));
         out.meshes
             .extend(gltf_scene.meshes.into_iter().map(|mesh| mesh.into()));
+        // for each mesh, also ensure that if any has joint then all will have joints
         out.nodes.extend(gltf_scene.nodes.into_iter().map(|node| {
             let GLTFNode {
                 mesh,
