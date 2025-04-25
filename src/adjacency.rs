8618ac43d07425a0e34ad3c012c00b08973c13dc
Julian Knodt <julianknodt@gmail.com>
2025-04-25 13:00:03 +0900

    Complete boundary loops and move to adjacency
    Also make boundary loops return the number of boundary loops there are in the mesh.

--- src/adjacency.rs
+++ src/adjacency.rs
@@ -1,5 +1,5 @@
 use super::Mesh;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 /// Structure for maintaining vertex adjacencies of a mesh with fixed topology.
 /// If the mesh is modified, this structure will no longer be valid.
@@ -16,6 +16,7 @@ impl Mesh {
         let mut nbrs: BTreeMap<usize, Vec<u32>> = BTreeMap::new();
         for f in &self.f {
             for [e0, e1] in f.edges() {
+                assert_ne!(e0, e1);
                 nbrs.entry(e0).or_default().push(e1 as u32);
                 nbrs.entry(e1).or_default().push(e0 as u32);
             }
@@ -47,5 +48,55 @@ impl VertexAdj {
         let cnt = cnt as usize;
 
         &self.adj[idx..idx + cnt]
+    }
+
+    /// Connectivity between boundary vertices (vert -> [prev, next])
+    /// Also returns the number of boundary loops present in this mesh.
+    pub fn boundary_loops(&self, m: &Mesh) -> (usize, BTreeMap<usize, [usize; 2]>) {
+        let mut bd_verts = m.boundary_vertices().collect::<BTreeSet<_>>();
+
+        let mut out = BTreeMap::new();
+        let mut num_loops = 0;
+        while let Some(s) = bd_verts.pop_first() {
+            num_loops += 1;
+            let nbrs = self.adj(s);
+            let next = *nbrs
+                .iter()
+                .find(|&&v| bd_verts.contains(&(v as usize)))
+                .unwrap();
+            let mut prev = *nbrs
+                .iter()
+                .find(|&&v| bd_verts.contains(&(v as usize)) && v != next)
+                .unwrap() as usize;
+            let next = next as usize;
+            assert_eq!(out.insert(s as usize, [prev, next]), None);
+            assert_eq!(out.insert(next, [s, usize::MAX]), None);
+            assert_eq!(out.insert(prev, [usize::MAX, s]), None);
+            prev = s;
+            let mut curr = next;
+            loop {
+                assert!(bd_verts.remove(&curr));
+                let nbrs = self.adj(curr);
+                let Some(&next) = nbrs.iter().find(|&&v| bd_verts.contains(&(v as usize))) else {
+                    break;
+                };
+                let next = next as usize;
+
+                assert_ne!(next, prev);
+                out.entry(curr).or_insert([usize::MAX; 2])[1] = next;
+                out.entry(next)
+                    .and_modify(|v| {
+                        v[0] = curr;
+                    })
+                    .or_insert([curr, usize::MAX]);
+                prev = curr;
+                curr = next;
+            }
+        }
+        assert!(out
+            .values()
+            .all(|&[v0, v1]| v0 != usize::MAX && v1 != usize::MAX));
+
+        (num_loops, out)
     }
 }
