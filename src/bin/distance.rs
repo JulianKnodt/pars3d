cfb151b46090e8d8ef1625bf762c55d2436ed143
Julian Knodt <julianknodt@gmail.com>
2025-05-03 17:01:59 +0900

    Add bin for distance between two meshes
    Utility for computing the distance between two meshes. Intended to replace a python binary I've
    been using for computing the distance between two meshes.

---
+++ src/bin/distance.rs
@@ -0,0 +1,113 @@
+#[cfg(not(feature = "kdtree"))]
+fn main() {
+    eprintln!(
+        r#"
+  _,    _,   ,_    _,
+ / \,  / \,  |_)  (_,
+'\_/  '\_/  '|     _)
+ '     '     '    '"#
+    );
+    eprintln!("Not compiled with KDTree support, exiting.");
+}
+
+#[cfg(feature = "kdtree")]
+fn main() {
+    use kdtree::KDTree;
+    use pars3d::{dist, length, load, F};
+
+    macro_rules! help {
+        ($( $extra: expr )?) => {{
+            $(
+              eprintln!($extra);
+            )?
+            eprintln!(
+                r#"Usage: <bin> <mesh a> <mesh b> <#samples:int> <stat file>
+- A tool for evaluating the distance between two meshes including their attributes."#
+            );
+            return;
+        }};
+    }
+    let mut src = None;
+    let mut dst = None;
+    let mut num_samples = None;
+    let mut stat = None;
+    for v in std::env::args().skip(1) {
+        let mut any = false;
+        for arg in [&mut src, &mut dst, &mut num_samples, &mut stat] {
+            if arg.is_some() {
+                continue;
+            }
+            any = true;
+            *arg = Some(v);
+            break;
+        }
+        if !any {
+            help!("Got too many arguments, expected 3 or 4 arguments");
+        };
+    }
+    let Some(src) = src else {
+        help!("Missing 1st argument, <mesh a>");
+    };
+    let Some(dst) = dst else {
+        help!("Missing 2nd argument, <mesh b>");
+    };
+    if src.starts_with("-") || dst.starts_with("-") {
+        help!("No flags are supported, assuming help");
+    }
+    let Some(num_samples) = num_samples.as_ref().and_then(|ns| ns.parse::<usize>().ok()) else {
+        help!("Did not get number of samples to add, instead got {num_samples:?}");
+    };
+
+    println!("[INFO]: Evaluating geometric distance between {src} & {dst}");
+
+    let a = load(&src)
+        .expect(&format!("Failed to load {}", src))
+        .into_flattened_mesh();
+    let b = load(&dst)
+        .expect(&format!("Failed to load {}", dst))
+        .into_flattened_mesh();
+
+    let a_aabb = a.aabb();
+    let b_aabb = b.aabb();
+
+    let diag_len = length(a_aabb.diag()).max(length(b_aabb.diag()));
+    assert!(diag_len > 0., "Both meshes are degenerate");
+
+    let mut i = a.v.len() as F;
+    let a_samples = a
+        .random_points_on_mesh(num_samples, || {
+            let v = (i * 389.21 + 0.348).cos();
+            i += v;
+            v.fract()
+        })
+        .map(|(fi, b)| a.f[fi].map_kind(|vi| a.v[vi]).from_barycentric(b));
+    let a_kdtree = KDTree::<(), 3, false, F>::new(
+        a.v.iter().copied().chain(a_samples).map(|v| (v, ())),
+        Default::default(),
+    );
+
+    let mut i = b.v.len() as F;
+    let b_samples = b
+        .random_points_on_mesh(num_samples, || {
+            let v = (i * 389.21 + 0.348).cos();
+            i += v;
+            v.fract()
+        })
+        .map(|(fi, bary)| b.f[fi].map_kind(|vi| b.v[vi]).from_barycentric(bary));
+    let b_kdtree = KDTree::<(), 3, false, F>::new(
+        b.v.iter().copied().chain(b_samples).map(|v| (v, ())),
+        Default::default(),
+    );
+
+    let mut a_to_b_dists = vec![];
+    for &p in a_kdtree.points() {
+        let (&nearest, _d, ()) = b_kdtree.nearest(&p).unwrap();
+        a_to_b_dists.push(dist(p, nearest) / diag_len);
+    }
+
+    let mut b_to_a_dists = vec![];
+    for &p in b_kdtree.points() {
+        let (&nearest, _d, ()) = a_kdtree.nearest(&p).unwrap();
+        b_to_a_dists.push(dist(p, nearest) / diag_len);
+    }
+}
