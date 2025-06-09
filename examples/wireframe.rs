143443a2cef8d7e4cab5c7c258cbf048efdd8393
Julian Knodt <julianknodt@gmail.com>
2025-06-09 14:40:07 +0900

    Update wireframe to take a width argument

--- examples/wireframe.rs
+++ examples/wireframe.rs
@@ -1,26 +1,54 @@
 #![feature(cmp_minmax)]
 
-use pars3d::{load, save};
+use pars3d::{load, save, F};
 use std::collections::BTreeSet;
 
 fn main() -> std::io::Result<()> {
     macro_rules! help {
         () => {{
             eprintln!("Outputs the wireframe of <arg1> to <arg2>");
-            eprintln!("Usage: <bin> src dst");
+            eprintln!("Usage: <bin> src dst [-w/--width <float>]");
             return Ok(());
         }};
     }
+
+    #[derive(PartialEq, Eq)]
+    pub enum State {
+        Empty,
+        Width,
+    }
+
+    let mut state = State::Empty;
     let mut src = None;
     let mut dst = None;
+    let mut width = 1.5e-3;
     for v in std::env::args().skip(1) {
-        if src.is_none() {
-            src = Some(v);
-        } else if dst.is_none() {
-            dst = Some(v)
-        } else {
-            help!();
-        };
+        match v.as_str() {
+            "-w" | "--width" => {
+                state = State::Width;
+                continue;
+            }
+            "-h" | "--help" => help!(),
+            _ => {}
+        }
+        match state {
+            State::Width => {
+                width = v.parse::<F>().unwrap();
+                state = State::Empty;
+            }
+            State::Empty => {
+                if src.is_none() {
+                    src = Some(v);
+                } else if dst.is_none() {
+                    dst = Some(v)
+                } else {
+                    help!();
+                };
+            }
+        }
+    }
+    if state != State::Empty {
+      help!();
     }
     let Some(src) = src else {
         help!();
@@ -41,7 +69,7 @@ fn main() -> std::io::Result<()> {
         edges.iter().copied(),
         |vi| m.v[vi],
         |[_, _]| [0.; 3],
-        1e-3,
+        width,
     );
 
     let out_mesh = pars3d::visualization::wireframe_to_mesh(wf);
