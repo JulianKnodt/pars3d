cf6e49969784c8ee8235c79a28230e5bfc711230
Julian Knodt <julianknodt@gmail.com>
2025-07-01 16:27:44 +0900

    Unify help message for convert

--- src/bin/convert.rs
+++ src/bin/convert.rs
@@ -3,27 +3,30 @@ use pars3d::{load, save};
 fn main() {
     let mut src = None;
     let mut dst = None;
+    macro_rules! help {
+        () => {{
+            eprintln!("[HELP]: Import and export a given mesh.");
+            eprintln!("Usage: <bin> src dst");
+            return;
+        }};
+    }
     for v in std::env::args().skip(1) {
         if src.is_none() {
             src = Some(v);
         } else if dst.is_none() {
             dst = Some(v)
         } else {
-            eprintln!("Usage: <bin> src dst");
-            return;
+            help!();
         };
     }
     let Some(src) = src else {
-        eprintln!("Usage: <bin> src dst");
-        return;
+        help!();
     };
     let Some(dst) = dst else {
-        eprintln!("Usage: <bin> src dst");
-        return;
+        help!();
     };
     if src.starts_with("-") || dst.starts_with("-") {
-        eprintln!("Usage: <bin> src dst");
-        return;
+        help!();
     }
     println!("[INFO]: {src} -> {dst}");
 
